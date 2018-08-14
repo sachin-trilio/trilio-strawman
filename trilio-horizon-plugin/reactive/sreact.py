83c31561642e9ba449a2ea6264c8eb9cf7dd3493
sachin-trilio <sachin.kulkarni@trilio.io>
2018-08-14 07:25:42 -0400

    Added handlers for start, stop

--- trilio-horizon-plugin/reactive/sreact.py
+++ trilio-horizon-plugin/reactive/sreact.py
@@ -1,11 +1,18 @@
 import os
 import subprocess
-from charms.reactive import when, when_not, set_flag, is_state, hook, remove_state
+from charms.reactive import when, when_not, set_flag, is_state, hook, remove_state, set_state
 from charmhelpers.core.hookenv import status_set, config
 
+@hook('install')
+def install_handler():
 
-@when_not('sreact.installed')
+    # Set the user defined "installing" state when this hook event occurs.
+    set_state('sreact.installing')
+
+@when('sreact.installing')
 def install_sreact():
+
+    status_set('maintenance', 'Installing...')
 
     #Push /usr/bin onto the start of $PATH from the hander for the specific subprocess call for the install script.
     #This will make pip to be called from host install rather than virtualenv.
@@ -20,7 +27,53 @@ def install_sreact():
 
     #Call install script to install the packages
     subprocess.check_call(['files/trilio/install', tv_version, tv_ip, horizon_path, webserver], env=s_env)
-    set_flag('sreact.installed')
+
+    status_set('active', 'TVM Horizon Plugin installed...')
+
+    #Remove the state "installing" since it's done
+    remove_state('sreact.installing')
+
+@hook('start')
+def start_handler():
+
+    # Set the user defined "starting" state when this hook event occurs.
+    set_state('sreact.starting')
+
+@when('sreact.starting')
+def start_sreact():
+
+    status_set('maintenance', 'Starting...')
+
+    #Call the script to re-start webserver
+    subprocess.check_call(['files/trilio/webserver-restart'])
+
+    status_set('active', 'Ready...')
+
+    #Remove the state "starting" since it's done
+    remove_state('sreact.starting')
+
+@hook('stop')
+def stop_handler():
+
+    # Set the user defined "stopping" state when this hook event occurs.
+    set_state('sreact.stopping')
+
+@when('sreact.stopping')
+def stop_sreact():
+
+    status_set('maintenance', 'Stopping...')
+
+    #Push /usr/bin onto the start of $PATH from the hander for the specific subprocess call for the install script.
+    #This will make pip to be called from host install rather than virtualenv.
+    s_env = os.environ.copy()
+    s_env['PATH'] = '/usr/bin:{}'.format(s_env['PATH'])
+    
+    #Call the script to stop and uninstll TVM Horizon Plugin
+    subprocess.check_call(['files/trilio/stop'], env=s_env)
+
+    #Remove the state "stopping" since it's done
+    remove_state('sreact.stopping')
+
 
 @hook('{requires:name}-relation-{joined,changed}')
 def changed_sreact(self):
